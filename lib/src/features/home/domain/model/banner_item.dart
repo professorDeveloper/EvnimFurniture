2789a547242a770b4639410c97b5df18baf03d28
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2026-04-26 15:50:07 +0500

    fixed banner_response.dart

--- lib/src/features/home/domain/model/banner_item.dart
+++ lib/src/features/home/domain/model/banner_item.dart
@@ -3,11 +3,17 @@ class BannerItem {
     required this.id,
     required this.imageUrl,
     this.furnitureMaterialId,
+    this.furnitureCombinationId,
     required this.order,
   });
 
   final String id;
   final String imageUrl;
   final String? furnitureMaterialId;
+  final String? furnitureCombinationId;
   final int order;
+
+  bool get hasLink =>
+      (furnitureMaterialId != null && furnitureMaterialId!.isNotEmpty) ||
+      (furnitureCombinationId != null && furnitureCombinationId!.isNotEmpty);
 }
